5aced3a6d0a03978fa45e31fec99ecd6f2bc81e9
DaggerES <gavazquez@gmail.com>
2016-02-16 11:24:59 +0100

    Update ReloadCam_Versions.py

--- ReloadCam_Versions.py
+++ ReloadCam_Versions.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
 Versions = {
     "ReloadCam_Arguments" : "1.0.0",
     "ReloadCam_Common" : "1.0.0",
