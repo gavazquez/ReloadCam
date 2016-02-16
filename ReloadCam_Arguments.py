d7ac4244d9f048c62b49c63b9a4483809d306737
DaggerES <gavazquez@gmail.com>
2016-02-16 14:10:15 +0100

    Create ReloadCam_Arguments.py

---
+++ ReloadCam_Arguments.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
+def GetVersion():
+    return 1
+
+Arguments = ['Mycccam','Satna','cccam4you','testious','freecline','allcam','cccamfree','cccamgenerator', 'toopCCcam', 'all']
