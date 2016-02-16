db3507fe236b483062aa268a562857911758f3a1
DaggerES <gavazquez@gmail.com>
2016-02-16 17:04:12 +0100

    Update ReloadCam_Arguments.py

--- ReloadCam_Arguments.py
+++ ReloadCam_Arguments.py
@@ -7,4 +7,22 @@
 def GetVersion():
     return 1
 
-Arguments = ['Mycccam','Satna','Cccam4you','Testious','Freecline','Allcam','Cccamfree','Cccamgenerator', 'Toopcccam']
+Arguments = [
+    'Mycccam',
+    'Satna',
+    'Cccam4you',
+    'Testious',
+    'Freecline',
+    'Allcam',
+    'Cccamfree',
+    'Cccamgenerator',
+    'Cccamcafard',
+    'Toopccam',
+    'Kacsat',
+    'Elaissaoui',
+    'Realtv1',
+    'Satunivers',
+    'Cccamgratis',
+    'Xhaka',
+    'Greencccamfree'
+]
