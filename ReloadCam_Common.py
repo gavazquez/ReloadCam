766798a751f08be9180d36f2362c6ca6a20576df
DaggerES <gavazquez@gmail.com>
2016-02-16 14:09:58 +0100

    Create ReloadCam_Common.py

---
+++ ReloadCam_Common.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
+import ReloadCam_Arguments, ReloadCam_Helper
+
+def GetVersion():
+    return 1
+
+class Server(object):
+    def GetUrl():
+        pass
+    def GetClines(self):
+        pass
+
+def WriteCccamFile(clines, append, check):
+    """Crea el archivo CCCam.cfg"""
+    import os, os.path
+
+    existingClines = []
+    clinesToWrite = []
+
+    if append and os.path.exists(cccamPath):
+        with open(cccamPath) as f:
+            existingClines = f.readlines()
+    
+    existingClines = filter(None, existingClines)
+
+    for cline in existingClines:
+        if check == False or (check == True and TestCline(cline) == True):
+            clinesToWrite.append(cline)
+
+    clinesToWrite = clinesToWrite + clines
+
+    clinesToWrite = SortClinesByPing(clinesToWrite)
+
+    file = open(cccamPath, 'w')
+
+    for cline in clinesToWrite:
+        file.write(cline + '\n')
+
+    file.close()
+
+    print "Finished refreshing the CCcam.cfg file!"
+
+def GetClinesByArgument(arguments, customClines):
+    """Lee los arguments y carga las clines pertinentes"""
+    import importlib
+
+    clines = []
+    clines += customClines #Primero agregamos las clines custom
+    moduleName = 'ReloadCam_' + argument #creamos el nombre del modulo que tenemos que importar ej:ReloadCam_Myccam
+
+    for argument in arguments:
+        my_module = importlib.import_module(moduleName) #Esta linea importa el modulo como si hicieramos un import <nombremodulo>
+        classInstance = getattr(my_module, argument)() #Creamos una instancia de ese modulo importado
+        clines += classInstance.GetClines() #Este metodo lo deben implementar todas las clases derivadas de "Server"
+
+    return clines
+
+def RestartCccam():
+    """Resetea el proceso de CCCam para que las nuevas clines se carguen"""
+    import time, os
+    os.system('killall ' + os.path.basename(cccamBin))
+    time.sleep(2)
+    os.system('rm -rf /tmp/*.info* /tmp/*.tmp*')
+    os.system(cccamBin + ' &')
+
+#endregion
