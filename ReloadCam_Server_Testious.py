ac14f4a08b0fd093a103e43eecc054d33c4320d8
DaggerES <gavazquez@gmail.com>
2016-02-16 17:10:33 +0100

    Create ReloadCam_Server_Testious.py

---
+++ ReloadCam_Server_Testious.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
+import ReloadCam_Main, ReloadCam_Helper
+
+def GetVersion():
+    return 1
+
+#Filename must start with Server, classname and argument must be the same!
+class Testious(ReloadCam_Main.Server):
+
+    def GetUrl(self):
+        return None
+
+    def GetClines(self):
+        print "Now getting Testious clines!"
+        return filter(None, self.__GetTestiousClines())
+
+    def __GetTestiousClines(self):
+        import re, time, datetime
+
+        clines = []
+
+        header = [('User-agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36')]
+        url = "http://www.testious.com/free-cccam-servers/" + datetime.date.today().strftime("%Y-%m-%d")
+        htmlCode = ReloadCam_Helper.GetHtmlCode(header, url)
+
+        regExpr = re.compile('([CN]:.*?)#.*\n<br>')
+        matches = regExpr.findall(htmlCode)
+
+        while len(matches) < 5:
+            yesterday = datetime.date.today() - datetime.timedelta( days = 1 )        
+            url = "http://www.testious.com/free-cccam-servers/" + yesterday.strftime("%Y-%m-%d")
+            htmlCode = ReloadCam_Helper.GetHtmlCode(header, url)
+            matches = regExpr.findall(htmlCode)
+
+        for i in range(0, 5):
+            if ReloadCam_Helper.TestCline(matches[i]):
+                clines.append(matches[i])
+
+        return clines;
