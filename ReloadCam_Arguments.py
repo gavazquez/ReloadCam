3b5125980bd6af20b155a790fdd037d9b7dcddb1
DaggerES <gavazquez@gmail.com>
2016-02-16 17:49:17 +0100

    Update ReloadCam_Arguments.py

--- ReloadCam_Arguments.py
+++ ReloadCam_Arguments.py
@@ -5,7 +5,7 @@
 #Creado por Dagger
 
 def GetVersion():
-    return 1
+    return 2
 
 Arguments = [
     'Mycccam',
@@ -24,5 +24,8 @@ Arguments = [
     'Satunivers',
     'Cccamgratis',
     'Xhaka',
-    'Greencccamfree'
+    'Greencccamfree',
+    'Jokercccam',
+    'Ultrahd',
+    'Seduct'
 ]
