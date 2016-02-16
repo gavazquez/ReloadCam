077d8aba6100715a3a0985ce482bc4a5b050e690
DaggerES <gavazquez@gmail.com>
2016-02-16 17:48:14 +0100

    Create ReloadCam_Server_Seduct.py

---
+++ ReloadCam_Server_Seduct.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
+import ReloadCam_Main, ReloadCam_Helper
+
+def GetVersion():
+    return 1
+
+#Filename must start with Server, classname and argument must be the same!
+class Seduct(ReloadCam_Main.Server):
+
+    def GetUrl(self):
+        #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
+        realUrl = ReloadCam_Helper.Decrypt('maanpH1wfOXK0OTE2F-eopusr8Hhk9nUkNekrWOxcbG14g==')
+        return realUrl
+
+    def GetClines(self):
+        print "Now getting Seduct clines!"
+        myccclineClines = []
+        myccclineClines.append(self.__GetSeductCline(1))
+        myccclineClines.append(self.__GetSeductCline(2))
+        myccclineClines.append(self.__GetSeductCline(3))
+        myccclineClines.append(self.__GetSeductCline(4))
+        myccclineClines.append(self.__GetSeductCline(5))
+        myccclineClines.append(self.__GetSeductCline(6))
+        return filter(None, myccclineClines)
+
+    def __GetSeductCline(self, serverNo):
+
+        htmlCode = ReloadCam_Helper.GetHtmlCode(None, self.GetUrl().format(serverNo))
+        cline = ReloadCam_Helper.FindClineInText(htmlCode, "([CN]:\s?\S+?\s+\d*\s+[0-9.]*\s?\w+)")
+
+        if cline != None and ReloadCam_Helper.TestCline(cline):
+            return cline
+        return None
