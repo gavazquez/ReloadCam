8d9adf5a4adbdd48564d314343a36c83c8de10e5
DaggerES <gavazquez@gmail.com>
2016-02-16 17:04:39 +0100

    Update and rename ReloadCam_Common.py to ReloadCam_Main.py

--- ReloadCam_Main.py
+++ ReloadCam_Main.py
@@ -48,7 +48,7 @@ def GetClinesByArgument(arguments, customClines):
     clines += customClines #Primero agregamos las clines custom
 
     for argument in arguments:
-        moduleName = 'ReloadCam_' + argument #creamos el nombre del modulo que tenemos que importar ej:ReloadCam_Myccam
+        moduleName = 'ReloadCam_Server_' + argument #creamos el nombre del modulo que tenemos que importar ej:ReloadCam_Myccam
         my_module = importlib.import_module(moduleName) #Esta linea importa el modulo como si hicieramos un import <nombremodulo>
         classInstance = getattr(my_module, argument)() #Creamos una instancia de ese modulo importado
         clines += classInstance.GetClines() #Este metodo lo deben implementar todas las clases derivadas de "Server"
@@ -66,5 +66,42 @@ def RestartCccam(path):
         os.system(path + ' &')
     else:
         print "ERROR! Cannot restart cccam! Restart manually or fix variable path cccamBin! Current value: " + path
+
+def Main(customClines, cccamPath, cccamBin):
+    import sys, os, optparse, ReloadCam_Arguments
+    clines = []
+
+    parser = optparse.OptionParser(description="Refrescador automatico de clines. Creado por Dagger")
+
+    possibleArguments = '%s' % ','.join(map(str, ReloadCam_Arguments.Arguments))
+
+    parser.add_option('-s', '--server', dest='web', action='append', choices=ReloadCam_Arguments.Arguments,
+        help='Especifica la web de la que quieres descargar las clines. Puedes repetir este parametro varias veces. \
+        Valores posibles: ' + possibleArguments)
+    
+    parser.add_option('-a', '--append', dest='append', default=False, action='store_true',
+        help='Mete las nuevas lineas al final sin sobreescribir el CCcam.cfg')
+
+    parser.add_option('-c', '--check', dest='check', default=False, action='store_true', 
+        help='Checkea las lineas antiguas del CCcam.cfg y las borra si no funcionan')
+
+    (opts, args) = parser.parse_args()
+
+    if opts.check and not opts.append:
+        print "Option check requires option append\n"
+        parser.print_help()
+        exit(-1)
+
+    clines = GetClinesByArgument(opts.web, customClines)
+
+    if len(clines) > 0:
+        print "Writing to the cccam.cfg!"
+        WriteCccamFile(clines, opts.append, opts.check, cccamPath)
+        print "Restarting cam!"
+        RestartCccam(cccamBin)
+        print "Finished!!!"
+    else :
+        print "ERROR!!!! NO CCCAMS LOADED!"
+    return;
 
 #endregion
