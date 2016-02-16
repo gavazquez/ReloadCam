e0ce8187141362a5fb3a45410dd0a6b8afd07c01
DaggerES <gavazquez@gmail.com>
2016-02-16 17:08:02 +0100

    Create ReloadCam_Server_Cccamgenerator.py

---
+++ ReloadCam_Server_Cccamgenerator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
+import ReloadCam_Main, ReloadCam_Helper
+
+def GetVersion():
+    return 1
+
+#Filename must start with Server, classname and argument must be the same!
+class Cccamgenerator(ReloadCam_Main.Server):
+
+    def GetUrl(self):
+        #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
+        realUrl = ReloadCam_Helper.Decrypt("maanpH1wfNXIz9DOy5agmKaktbzkk8_ezpOYl6GZtaLB4deb1sbYX6KbpA==")
+        return realUrl
+
+    def GetClines(self):
+        print "Now getting Cccamgenerator clines!"
+        cccamFreeClines = []
+        cccamFreeClines.append(self.__GetCccamfreeCline())
+        return filter(None, cccamFreeClines)
+
+    def __GetCccamfreeCline(self):
+        htmlCode = ReloadCam_Helper.GetHtmlCode(None, self.GetUrl())
+        cline = ReloadCam_Helper.FindStandardClineInText(htmlCode)
+        if cline != None and ReloadCam_Helper.TestCline(cline):
+            return cline
+        return NoneReloadCam_Server_Cccamgenerator
