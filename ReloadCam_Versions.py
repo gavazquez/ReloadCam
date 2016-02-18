e7be95f58e5953b1f531c4b4517f29d0121547a5
DaggerES <gavazquez@gmail.com>
2016-02-19 00:27:31 +0100

    Update ReloadCam_Versions.py

--- ReloadCam_Versions.py
+++ ReloadCam_Versions.py
@@ -7,7 +7,7 @@
 Versions = {
     "ReloadCam_Arguments" : 2,
     "ReloadCam_Helper" : 3,
-    "ReloadCam_Main" : 2,
+    "ReloadCam_Main" : 3,
     "ReloadCam_Server_Allcam" : 1,
     "ReloadCam_Server_Cccam4you" : 1,
     "ReloadCam_Server_Cccamcafard" : 1,
