787ff5cc24e8bf9552df31c389c7ad3f61265659
DaggerES <gavazquez@gmail.com>
2016-02-18 14:00:07 +0100

    Update ReloadCam.py

--- ReloadCam.py
+++ ReloadCam.py
@@ -153,7 +153,7 @@ def Main():
     else:
         ReloadCam_Main.Main(GetCustomClines(), cccamPath, cccamBin)
 
-    ReloadCam_Main.Cleanup(GetCustomClines(), cccamPath, cccamBin)
+    ReloadCam_Main.Cleanup()
 
 #endregion
 
