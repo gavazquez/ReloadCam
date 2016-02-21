8a6b9b1965a8bed9cf1d74ab9934832064dcaa77
DaggerES <gavazquez@gmail.com>
2016-02-21 15:28:34 +0100

    Create ReloadCamUndro.py

---
+++ ReloadCamUndro.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#ReloadCamUndro creado por Zurder
+
+def ReloadCamUndro():
+   # Read in the file
+   with open(cccamPath, 'r') as file :
+      
+      # Replace the target string 
+      lines = [( '' + line.replace('C: ', 'C:').replace('|1|0', '').replace('DEFAULT:1', '#').replace('DEFAULT:0', '#').replace(' ', '|').rstrip('\n') + '|1|0' + "\n" ) for line in open(cccamPath) ]
+
+   # Write the file out again
+   with open(cccamPath, 'w') as file: 
+   
+      #IKS
+      if lines == 0:
+         file.write('DEFAULT:1\n')
+      #CCCAM
+      file.writelines(lines)
+
+
+   import time
+   # Reinicio de Spring.apk (al salir de qpython cargará de nuevo listas, canales, ...)
+   os.system("adb shell am force-stop com.dvb.spring.home")
+   
+   print "Saliendo ..."
+   
+   # Salimos de qpython
+   time.sleep(1.5)
+   os.system("adb shell am force-stop com.hipipal.qpyplus")
