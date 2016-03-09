fe154315d596c16bd05132ecffbf6de38e6ca9d9
DaggerES <gavazquez@gmail.com>
2016-03-09 19:08:31 +0100

    Update DELETED_ReloadCam_Server_Realtv1.py

--- DELETED_ReloadCam_Server_Realtv1.py
+++ DELETED_ReloadCam_Server_Realtv1.py
@@ -7,7 +7,7 @@
 import ReloadCam_Main, ReloadCam_Helper
 
 def GetVersion():
-    return 0
+    return 1
 
 #Filename must start with Server, classname and argument must be the same!
 class Realtv1(ReloadCam_Main.Server):
