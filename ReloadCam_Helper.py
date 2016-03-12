da4bcc54cfd64feb1102d5b276c9f856fb41c2ef
Gabriel <gavazquez@gmail.com>
2016-03-12 10:10:21 +0100

    Update ReloadCam_Helper.py

--- ReloadCam_Helper.py
+++ ReloadCam_Helper.py
@@ -130,7 +130,7 @@ def TestNLine(nline):
         return False;
 
     testSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    testSocket.settimeout(3)
+    testSocket.settimeout(10)
 
     try:
         host = match.group(1)
