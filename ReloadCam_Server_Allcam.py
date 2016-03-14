35d4f5e75b87d50b72dd88317d146d9f92fdc252
Gabriel <gavazquez@gmail.com>
2016-03-14 15:12:08 +0100

    Update ReloadCam_Server_Allcam.py

--- ReloadCam_Server_Allcam.py
+++ ReloadCam_Server_Allcam.py
@@ -7,14 +7,17 @@
 import ReloadCam_Main, ReloadCam_Helper
 
 def GetVersion():
-    return 1
+    return 2
 
 #Filename must start with Server, classname and argument must be the same!
 class Allcam(ReloadCam_Main.Server):
 
-    def GetUrl(self):
+    def GetUrl(self, serverNo):
         #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
-        realUrl = ReloadCam_Helper.Decrypt("maanpH1wfOnc453C0J2VlpekrnvV1Nme1MmjqK5kwLN74s3c")
+        if serverNo <= 3:
+            realUrl = ReloadCam_Helper.Decrypt("maanpH1wfOnc453C0J2VlpekrnvV1Nme1MmjqK5kwLN74s3c").format(serverNo)
+        else:
+            realUrl = ReloadCam_Helper.Decrypt("maanpH1wfOnc453C0J2VlpekrnvV1Nme1MmjqK5kwG-92tU=").format(serverNo)
         return realUrl
 
     def GetClines(self):
@@ -28,7 +31,7 @@ class Allcam(ReloadCam_Main.Server):
         return filter(None, allcamClines)
 
     def __GetAllcamCline(self, serverNo):
-        htmlCode = ReloadCam_Helper.GetHtmlCode(None, self.GetUrl().format(serverNo))
+        htmlCode = ReloadCam_Helper.GetHtmlCode(None, self.GetUrl(serverNo))
         cline = ReloadCam_Helper.FindStandardClineInText(htmlCode)
         if cline != None and ReloadCam_Helper.TestCline(cline):
             return cline
