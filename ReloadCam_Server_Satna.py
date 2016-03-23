001ccfb2b10f2ad2eff3aa077da44c570e6021e8
Gabriel <gavazquez@gmail.com>
2016-03-23 13:09:13 +0100

    Update ReloadCam_Server_Satna.py

--- ReloadCam_Server_Satna.py
+++ ReloadCam_Server_Satna.py
@@ -7,7 +7,7 @@
 import ReloadCam_Main, ReloadCam_Helper
 
 def GetVersion():
-    return 1
+    return 2
 
 #Filename must start with Server, classname and argument must be the same!
 class Satna(ReloadCam_Main.Server):
@@ -26,7 +26,9 @@ class Satna(ReloadCam_Main.Server):
         satnaClines.append(self.__GetSatnaCline(4))
         satnaClines.append(self.__GetSatnaCline(5))
         satnaClines.append(self.__GetSatnaCline(6))
-        return filter(None, satnaClines)
+        satnaClines = filter(None, satnaClines)
+        if len(satnaClines) == 0: print "No Satna lines retrieved"
+        return satnaClines
 
     def __GetSatnaCline(self, serverNo):
         htmlCode = ReloadCam_Helper.GetHtmlCode(None, self.GetUrl().format(serverNo))
