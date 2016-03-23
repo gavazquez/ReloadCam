99eb42dc72d3b2ad1e0310c4480c67ed2dcff106
Gabriel <gavazquez@gmail.com>
2016-03-23 13:46:14 +0100

    Update ReloadCam_Server_Cccamgratis.py

--- ReloadCam_Server_Cccamgratis.py
+++ ReloadCam_Server_Cccamgratis.py
@@ -7,15 +7,14 @@
 import ReloadCam_Main, ReloadCam_Helper
 
 def GetVersion():
-    return 2
+    return 3
 
 #Filename must start with Server, classname and argument must be the same!
 class Cccamgratis(ReloadCam_Main.Server):
 
     def GetUrl(self):
         #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
-        "http://cccam.gratis/free/index.php"
-        realUrl = ReloadCam_Helper.Decrypt("maanpH1wfOXG4OTPzaeXpadxpbHg2Jrdxthgm6GYqLl74s3c")
+        realUrl = ReloadCam_Helper.Decrypt('maanpH1wfNXIz9DOkpiklKistHzY19HUkM2flpiscbG14g==')
         return realUrl
 
     def GetClines(self):
