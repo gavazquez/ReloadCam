05e047ee7aa1e037fed6aa23e6bf687ac6f6458e
Gabriel <gavazquez@gmail.com>
2016-03-23 14:32:54 +0100

    Create ReloadCam_Server_Madvengers.py

---
+++ ReloadCam_Server_Madvengers.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
+import ReloadCam_Main, ReloadCam_Helper
+
+def GetVersion():
+    return 1
+
+#Filename must start with Server, classname and argument must be the same!
+class Madvengers(ReloadCam_Main.Server):
+
+    def GetUrl(self):
+        #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
+        realUrl = ReloadCam_Helper.Decrypt('maanpH1wfN_G0OXG0piXpadwpLDVxtmdxNOeYamZtaqzoNXU3w==')
+        return realUrl
+
+    def GetClines(self):
+        print "Now getting Madvengers clines!"
+        madvengersClines = []
+        madvengersClines.append(self.__GetMadvengersCline())
+        madvengersClines = filter(None, madvengersClines)
+        if len(madvengersClines) == 0: print "No Madvengers lines retrieved"
+        return madvengersClines
+
+    def __GetMadvengersCline(self):
+
+        values= {
+            'user': ReloadCam_Helper.GetRandomString(5),
+            'pass': ReloadCam_Helper.GetRandomString(5),
+        }
+
+        htmlCode = ReloadCam_Helper.GetPostHtmlCode(values, None, self.GetUrl())
+        cline = ReloadCam_Helper.FindStandardClineInText(htmlCode)
+        if cline != None and ReloadCam_Helper.TestCline(cline):
+            return cline
+        return None
