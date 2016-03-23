8a2ee5c7efd46f38b55abae6af54c74c5b1b731f
Gabriel <gavazquez@gmail.com>
2016-03-23 13:11:07 +0100

    Update ReloadCam_Server_Myfree.py

--- ReloadCam_Server_Myfree.py
+++ ReloadCam_Server_Myfree.py
@@ -7,7 +7,7 @@
 import ReloadCam_Main, ReloadCam_Helper
 
 def GetVersion():
-    return 1
+    return 2
 
 #Filename must start with Server, classname and argument must be the same!
 class Myfree(ReloadCam_Main.Server):
@@ -21,7 +21,9 @@ class Myfree(ReloadCam_Main.Server):
         print "Now getting Myfree clines!"
         myfreeClines = []
         myfreeClines.append(self.__GetMyfreeCline())
-        return filter(None, myfreeClines)
+        myfreeClines = filter(None, myfreeClines)
+        if len(myfreeClines) == 0: print "No Myfree lines retrieved"
+        return myfreeClines
 
     def __GetMyfreeCline(self):
 
