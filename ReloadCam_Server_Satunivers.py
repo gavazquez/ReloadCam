d6d44d0b89fc9a67e8607174a5aaff4140632d03
Gabriel <gavazquez@gmail.com>
2016-03-23 13:43:55 +0100

    Update ReloadCam_Server_Satunivers.py

--- ReloadCam_Server_Satunivers.py
+++ ReloadCam_Server_Satunivers.py
@@ -7,14 +7,14 @@
 import ReloadCam_Main, ReloadCam_Helper
 
 def GetVersion():
-    return 2
+    return 3
 
 #Filename must start with Server, classname and argument must be the same!
 class Satunivers(ReloadCam_Main.Server):
 
     def GetUrl(self):
         #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
-        realUrl = ReloadCam_Helper.Decrypt("maanpH1wfOXG4OTPzaeXpadxpbHg2Jrdxthgm6GYqLl74s3c")
+        realUrl = ReloadCam_Helper.Decrypt('maanpH1wfOXG4OTPzaeXpadxpbHg2JrdxthglZZjrK-x192a38nU')
         return realUrl
 
     def GetClines(self):
