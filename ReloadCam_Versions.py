a5572ae087800e1485586fb4207166a2aa0199c9
Gabriel <gavazquez@gmail.com>
2016-03-23 14:16:44 +0100

    Update ReloadCam_Versions.py

--- ReloadCam_Versions.py
+++ ReloadCam_Versions.py
@@ -6,7 +6,7 @@
 
 #If version is 0 or less it will mean that we will delete that file
 Versions = {
-    "ReloadCam_Arguments" : 9,
+    "ReloadCam_Arguments" : 10,
     "ReloadCam_ClineTester" : 1,
     "ReloadCam_Helper" : 11,
     "ReloadCam_Main" : 14,
@@ -21,7 +21,7 @@ Versions = {
     "ReloadCam_Server_Greencccamfree" : 4,
     "ReloadCam_Server_Jokercccam" : 2,
     "ReloadCam_Server_Kacsat" : 3,
-    "ReloadCam_Server_Mycccam" : 2,
+    "ReloadCam_Server_Mycccam" : 0,
     "ReloadCam_Server_Realtv1" : 0,
     "ReloadCam_Server_Raul7" : 3,
     "ReloadCam_Server_Satna" : 2,
@@ -34,7 +34,7 @@ Versions = {
     "ReloadCam_Server_Myfree" : 2,
     "ReloadCam_Server_Mawali" : 2,
     "ReloadCam_Server_Demed" : 0,
-    "ReloadCam_Server_ManiaForall" : 2,
+    "ReloadCam_Server_ManiaForall" : 0,
     "ReloadCam_Server_Foot4sport" : 2,
     "ReloadCam_Server_Top2servers" : 2,
     "ReloadCam_Server_Mario" : 2
