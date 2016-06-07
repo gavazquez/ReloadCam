f44490dd692b9d619db768da20b797784403cefb
Gabriel <gavazquez@gmail.com>
2016-06-07 10:28:38 +0200

    Update ReloadCam_Versions.py

--- ReloadCam_Versions.py
+++ ReloadCam_Versions.py
@@ -8,7 +8,7 @@
 Versions = {
     "ReloadCam_Arguments" : 15,
     "ReloadCam_ClineTester" : 4,
-    "ReloadCam_Helper" : 16,
+    "ReloadCam_Helper" : 17,
     "ReloadCam_Main" : 19,
     "ReloadCam_Server_Allcam" : 5,
     "ReloadCam_Server_Cccam4you" : 6,
