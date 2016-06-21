11fa115d94aaef4207314683e438ada3403c1195
Gabriel <gavazquez@gmail.com>
2016-06-22 00:13:55 +0200

    Update ReloadCam_Versions.py

--- ReloadCam_Versions.py
+++ ReloadCam_Versions.py
@@ -6,11 +6,11 @@
 
 #If version is 0 or less it will mean that we will delete that file
 Versions = {
-    "ReloadCam_Arguments" : 17,
+    "ReloadCam_Arguments" : 18,
     "ReloadCam_ClineTester" : 4,
     "ReloadCam_Helper" : 19,
     "ReloadCam_NlineTester" : 6,
-    "ReloadCam_Main" : 24,
+    "ReloadCam_Main" : 25,
     "ReloadCam_Server_Allcam" : 6,
     "ReloadCam_Server_Cccam4you" : 6,
     "ReloadCam_Server_Cccamcafard" : 7,
