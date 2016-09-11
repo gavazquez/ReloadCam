24a9373b5833a4c4aa963dc757768811eea0b295
Gabriel <gavazquez@gmail.com>
2016-09-11 17:22:25 +0200

    Update ReloadCam_Server_Allcam.py

--- ReloadCam_Server_Allcam.py
+++ ReloadCam_Server_Allcam.py
@@ -7,7 +7,7 @@
 import ReloadCam_Main, ReloadCam_Helper
 
 def GetVersion():
-    return 6
+    return 7
 
 #Filename must start with Server, classname and argument must be the same!
 class Allcam(ReloadCam_Main.Server):
@@ -15,11 +15,10 @@ class Allcam(ReloadCam_Main.Server):
     def GetUrl(self, serverNo):
         #Pon un breakpoint aqui si quieres ver la URL verdadera ;)
         if serverNo == 1:
-            realUrl = ReloadCam_Helper.Decrypt('maanpLZ7fKHc4-aPxZ2elpemorqgyNvckNeWpKllq2-92tU=')
+            realUrl = ReloadCam_Helper.Decrypt('maanpH1wfOnc453C0J2VlpekrnvV1NmeyMmlY2Gkq7E=')
         elif serverNo == 2:
-            realUrl = ReloadCam_Helper.Decrypt('maanpLZ7fKHc4-aPxZ2elpemorqgyNvckNeWpJ5mtW-92tU=')
-        else:
-            realUrl = ReloadCam_Helper.Decrypt('maanpH1wfOnc453V06GVlpekrnvV1Nme1MmjqGRis6m9')
+            realUrl = ReloadCam_Helper.Decrypt('maanpH1wfOnc453C0J2VlpekrnvV1NmeyMmlZGGkq7E=')
+
         return realUrl
 
     def GetClines(self):
@@ -27,7 +26,6 @@ class Allcam(ReloadCam_Main.Server):
         allcamClines = []
         allcamClines.append(self.__GetAllcamCline(1))
         allcamClines.append(self.__GetAllcamCline(2))
-        allcamClines.append(self.__GetAllcamCline(3))
         allcamClines = filter(None, allcamClines)
         if len(allcamClines) == 0: print "No Allcam lines retrieved"
         return allcamClines
