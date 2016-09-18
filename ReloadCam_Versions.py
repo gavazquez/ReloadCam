7b7b3d71575cb6aeba53cef380f93cbdb2002103
DaggerES <gavazquez@gmail.com>
2016-09-18 22:05:45 +0200

    m

--- ReloadCam_Versions.py
+++ ReloadCam_Versions.py
@@ -1,48 +1,48 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- 
-
-#Refrescador automatico de clines
-#Creado por Dagger
-
-#If version is 0 or less it will mean that we will delete that file
-Versions = {
-    "ReloadCam_Arguments" : 20,
-    "ReloadCam_ClineTester" : 5,
-    "ReloadCam_Helper" : 20,
-    "ReloadCam_NlineTester" : 7,
-    "ReloadCam_Main" : 31,
-    "ReloadCam_Importlib" : 1,
-    "ReloadCam_Server_Allcam" : 8,
-    "ReloadCam_Server_Cccam4you" : 7,
-    "ReloadCam_Server_Cccamcafard" : 8,
-    "ReloadCam_Server_Cccamfree" : 4,
-    "ReloadCam_Server_Cccamgenerator" : 6,
-    "ReloadCam_Server_Cccamgratis" : 3,
-    "ReloadCam_Server_Elaissaoui" : 5,
-    "ReloadCam_Server_Freecline" : 5,
-    "ReloadCam_Server_Fullmonsters" : 3,
-    "ReloadCam_Server_Greencccamfree" : 4,
-    "ReloadCam_Server_Hacksat" : 2,
-    "ReloadCam_Server_Jokercccam" : 4,
-    "ReloadCam_Server_Kacsat" : 0,
-    "ReloadCam_Server_Mycccam" : 0,
-    "ReloadCam_Server_Realtv1" : 0,
-    "ReloadCam_Server_Raul7" : 6,
-    "ReloadCam_Server_Satna" : 0,
-    "ReloadCam_Server_Satunivers" : 4,
-    "ReloadCam_Server_Seduct" : 4,
-    "ReloadCam_Server_Testious" : 5,
-    "ReloadCam_Server_Toopccam" : 4,
-    "ReloadCam_Server_Ultrahd" : 0,
-    "ReloadCam_Server_Xhaka" : 3,
-    "ReloadCam_Server_Myfree" : 2,
-    "ReloadCam_Server_Mawali" : 0,
-    "ReloadCam_Server_Demed" : 0,
-    "ReloadCam_Server_ManiaForall" : 0,
-    "ReloadCam_Server_Foot4sport" : 0,
-    "ReloadCam_Server_Top2servers" : 2,
-    "ReloadCam_Server_Mario" : 5,
-    "ReloadCam_Server_Madvengers" : 0,
-    "ReloadCam_Server_Cccamgenerador" : 1,
-    "ReloadCam_Server_Kingcccam" : 1
-}
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Refrescador automatico de clines
+#Creado por Dagger
+
+#If version is 0 or less it will mean that we will delete that file
+Versions = {
+    "ReloadCam_Arguments" : 20,
+    "ReloadCam_ClineTester" : 5,
+    "ReloadCam_Helper" : 20,
+    "ReloadCam_NlineTester" : 7,
+    "ReloadCam_Main" : 31,
+    "ReloadCam_Importlib" : 1,
+    "ReloadCam_Server_Allcam" : 8,
+    "ReloadCam_Server_Cccam4you" : 7,
+    "ReloadCam_Server_Cccamcafard" : 8,
+    "ReloadCam_Server_Cccamfree" : 4,
+    "ReloadCam_Server_Cccamgenerator" : 6,
+    "ReloadCam_Server_Cccamgratis" : 3,
+    "ReloadCam_Server_Elaissaoui" : 5,
+    "ReloadCam_Server_Freecline" : 5,
+    "ReloadCam_Server_Fullmonsters" : 3,
+    "ReloadCam_Server_Greencccamfree" : 4,
+    "ReloadCam_Server_Hacksat" : 2,
+    "ReloadCam_Server_Jokercccam" : 4,
+    "ReloadCam_Server_Kacsat" : 0,
+    "ReloadCam_Server_Mycccam" : 0,
+    "ReloadCam_Server_Realtv1" : 0,
+    "ReloadCam_Server_Raul7" : 6,
+    "ReloadCam_Server_Satna" : 0,
+    "ReloadCam_Server_Satunivers" : 4,
+    "ReloadCam_Server_Seduct" : 4,
+    "ReloadCam_Server_Testious" : 5,
+    "ReloadCam_Server_Toopccam" : 4,
+    "ReloadCam_Server_Ultrahd" : 0,
+    "ReloadCam_Server_Xhaka" : 3,
+    "ReloadCam_Server_Myfree" : 2,
+    "ReloadCam_Server_Mawali" : 0,
+    "ReloadCam_Server_Demed" : 0,
+    "ReloadCam_Server_ManiaForall" : 0,
+    "ReloadCam_Server_Foot4sport" : 0,
+    "ReloadCam_Server_Top2servers" : 2,
+    "ReloadCam_Server_Mario" : 5,
+    "ReloadCam_Server_Madvengers" : 0,
+    "ReloadCam_Server_Cccamgenerador" : 1,
+    "ReloadCam_Server_Kingcccam" : 1
+}
